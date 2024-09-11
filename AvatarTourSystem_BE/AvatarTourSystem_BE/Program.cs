40d9ac0ff06ea0a99c6c94e6387d2307b7dabe8d
Phuoc Thinh <phuocthinhlkr@gmail.com>
2024-09-12 01:10:17 +0700

    test

--- AvatarTourSystem_BE/AvatarTourSystem_BE/Program.cs
+++ AvatarTourSystem_BE/AvatarTourSystem_BE/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//WORKFLOW test 
+//WORKFLOW test 2
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
