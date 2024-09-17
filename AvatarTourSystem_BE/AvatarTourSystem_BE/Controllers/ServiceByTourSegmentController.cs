06a5eb584d5719b8df44c6b4ee15b7f921e54c7f
Phuoc Thinh <phuocthinhlkr@gmail.com>
2024-09-17 20:24:18 +0700

    CRUD ServiceByTourSegment

---
+++ AvatarTourSystem_BE/AvatarTourSystem_BE/Controllers/ServiceByTourSegmentController.cs
@@ -0,0 +1,82 @@
+﻿using BusinessObjects.ViewModels.ServiceByTourSegment;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace AvatarTourSystem_BE.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ServiceByTourSegmentController : ControllerBase
+    {
+        private readonly IServiceByTourSegmentService _serviceByTourSegmentService;
+
+        public ServiceByTourSegmentController(IServiceByTourSegmentService serviceByTourSegmentService)
+        {
+            _serviceByTourSegmentService = serviceByTourSegmentService;
+        }
+
+        [HttpGet("active")]
+        public async Task<IActionResult> GetListActiveServiceByTourSegmentsAsync()
+        {
+            var result = await _serviceByTourSegmentService.GetActiveServiceByTourSegmentsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetListServiceByTourSegmentsAsync()
+        {
+            var result = await _serviceByTourSegmentService.GetServiceByTourSegmentsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetServiceByTourSegmentById(string id)
+        {
+            var result = await _serviceByTourSegmentService.GetServiceByTourSegmentByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateServiceByTourSegmentAsync([FromForm] ServiceByTourSegmentCreateModel createModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var result = await _serviceByTourSegmentService.CreateServiceByTourSegmentAsync(createModel);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateServiceByTourSegmentAsync([FromForm] ServiceByTourSegmentUpdateModel updateModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+
+                var result = await _serviceByTourSegmentService.UpdateServiceByTourSegmentAsync(updateModel);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteServiceByTourSegment(string id)
+        {
+            var result = await _serviceByTourSegmentService.DeleteServiceByTourSegment(id);
+            return Ok(result);
+        }
+    }
+}
